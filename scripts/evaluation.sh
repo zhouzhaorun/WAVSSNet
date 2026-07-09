308d8edeebf4bb796242845e3b790eb6a2b51485
zzr217 <zhouzhaorun1229@gmail.com>
2026-07-09 19:16:06 +0800

    Update evaluation.sh

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -3,14 +3,14 @@ clear
 
 # -------------------------------- iHarmony4 -------------------------------- # 
 # CUDA_VISIBLE_DEVICES=0 python evaluation/ih_evaluation.py \
-# --dataroot /data0/zzr/datasets/iHarmony4/ \
+# --dataroot /path/to/iHarmony4/ \
 # --result_root  results/iHarmony4/ \
 # --evaluation_type our \
 # --dataset_name ALL
 
 
 CUDA_VISIBLE_DEVICES=0 python evaluation/ih_evaluation_csv.py \
---dataroot /data0/zzr/datasets/iHarmony4/ \
+--dataroot /path/to/iHarmony4/ \
 --result_root  results/iHarmony4/ \
 --evaluation_type our \
 --dataset_name ALL 
@@ -18,14 +18,14 @@ CUDA_VISIBLE_DEVICES=0 python evaluation/ih_evaluation_csv.py \
 
 # -------------------------------- ccHarmony -------------------------------- # 
 # CUDA_VISIBLE_DEVICES=0 python evaluation/ih_evaluation.py \
-# --dataroot /data0/zzr/datasets/ccHarmony/ \
+# --dataroot /path/to/ccHarmony/ \
 # --result_root  results/ccHarmony/ \
 # --evaluation_type our \
 # --dataset_name ALL
 
 
 # CUDA_VISIBLE_DEVICES=0 python evaluation/ih_evaluation_csv.py \
-# --dataroot /data0/zzr/datasets/ \
+# --dataroot /path/to/datasets/ \
 # --result_root  results/ccHarmony/ \
 # --evaluation_type our \
 # --dataset_name ccHarmony 
